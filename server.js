b36b9465c708b0e563613ae679fa5aa924d92318
Alfrugo <diez_alfredo@hotmail.com>
2023-12-07 08:26:28 -0600

    delete

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(
   })
 );
 
+console.log('updated server.js');
+
 // Middleware to check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
   if (req.session && req.session.username) {
